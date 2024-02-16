569d5e1d5130c4ba4455401c9c53f809677b91f3
Suleymanguly Malikov <suleymanmalikov03@gmail.com>
2024-02-16 17:45:52 +0100

    adding server and client

---
+++ server/src/main/java/com/example/uniproject1/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.example.uniproject1.repository;
+
+import com.example.uniproject1.model.Student;
+import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
+}
---
+++ server/src/main/java/com/example/uniproject1/service/IStudentService.java
@@ -0,0 +1,14 @@
+package com.example.uniproject1.service;
+
+import com.example.uniproject1.model.Student;
+
+import java.util.List;
+
+public interface IStudentService {
+    Student addStudent(Student student);
+    List<Student> getStudents();
+    Student updateStudent(Student student, Long id);
+    Student getStudentById(Long id);
+    void deleteStudent(Long id);
+
+}
